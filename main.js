04d47ae835bef3eb3b039731877f3311162e160d
caqu <110166051+Samniq@users.noreply.github.com>
2023-05-04 07:28:09 +0200

    New options.
    New options:
    multiply,
    divide,
    power.

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const btnIncrease = document.getElementById("increase");
 const btnDecrease = document.getElementById("decrease");
 const btnReset = document.getElementById("reset");
+const btnMultiply = document.getElementById("multiply");
+const btnDivide = document.getElementById("divide");
+const btnPower = document.getElementById("power");
 const txt = document.getElementById("count-txt");
-
 let count = 0;
 
 btnIncrease.addEventListener("click", increase);
 btnDecrease.addEventListener("click", decrease);
+btnMultiply.addEventListener("click", multiply);
+btnDivide.addEventListener("click", divide);
+btnPower.addEventListener("click", power);
 btnReset.addEventListener("click", reset);
 
 function changeColor() {
@@ -31,6 +36,22 @@ function decrease() {
   changeColor();
 }
 
+function multiply() {
+  count = count * 2;
+  txt.innerHTML = count;
+  changeColor();
+}
+function power() {
+  count = count ** 2;
+  txt.innerHTML = count;
+  changeColor();
+}
+
+function divide() {
+  count = count / 2;
+  txt.innerHTML = count;
+  changeColor();
+}
 function reset() {
   count = 0;
   txt.innerHTML = count;
